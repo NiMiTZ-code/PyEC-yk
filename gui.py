df9687b210ad33bb4981a3f730576dc9931636f4
HoniMoni-07 <monimedrek@gmail.com>
2026-05-08 20:07:44 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -18,7 +18,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Analiza Krzywych Mieszania PK")
-        self.resize(1000, 600)
+        self.resize(1200, 750)
         
         self.processor = DataProcessor()
         self.checked_channels = []
